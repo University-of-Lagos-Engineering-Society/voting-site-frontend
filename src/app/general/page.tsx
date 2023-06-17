ab1c01fe9c30a124601645e9c1dbfa19557e8732
Udoagwa Franklin <frankudoags@gmail.com>
2023-06-17 10:35:59 +0100

    fix scroll bug

--- src/app/general/page.tsx
+++ src/app/general/page.tsx
@@ -76,7 +76,7 @@ const GeneralCategory = () => {
   }
 
   return (
-    <main className="relative flex min-h-screen xl:h-screen overflow-hidden flex-col bg-[#141414] px-4 py-6 text-white">
+    <main className="relative flex min-h-screen flex-col bg-[#141414] px-4 py-6 text-white">
       <div className="max-w-7xl mx-auto w-full">
         <Link href={"/categories"} className="text-sm lg:text-xl font-medium text-[#E1544A] work-sans mt-6 pb-4 lg:pb-6">
           GENERAL CATEGORY
